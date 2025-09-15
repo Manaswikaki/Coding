cca74d9546be166f05edb6cd3681cc6137d4c778
Manaswi <155607245+Manaswikaki@users.noreply.github.com>
2025-09-15 19:11:28 +0530

    Added solution - LeetHub

---
+++ 878955--GFG/878955-.py
@@ -0,0 +1,10 @@
+#User function Template for python3
+
+arr = tuple(map(int, input().split()))
+x = int(input())
+
+########### Write your code below ###############
+# Print the index of x in arr
+
+########### Write your code above ###############
+print(arr.index(x))
