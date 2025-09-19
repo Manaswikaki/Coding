a2247d6636545ead220e9a925441e8713b4e78f7
Manaswi <155607245+Manaswikaki@users.noreply.github.com>
2025-09-19 19:59:47 +0530

    Added solution - LeetHub

---
+++ 706674--GFG/706674-.py
@@ -0,0 +1,14 @@
+def insert(s, element):
+
+     #your code here
+     s.add(element)
+
+def remove_from_set(s, element):
+
+      #your code here
+     s.remove(element)
+
+def sum_set(s):
+
+     #your code here
+    return sum(s)
