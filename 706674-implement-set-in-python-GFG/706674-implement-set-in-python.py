1634d8af4dfd66de4b8d59196bd18a443c92b877
Manaswi <155607245+Manaswikaki@users.noreply.github.com>
2025-09-23 19:51:18 +0530

    Added solution - LeetHub

--- 706674-implement-set-in-python-GFG/706674-implement-set-in-python.py
+++ 706674-implement-set-in-python-GFG/706674-implement-set-in-python.py
@@ -1,28 +1,14 @@
-#User function Template for python3
-
-# Function to insert
-# no return should be there, and no print statement
 def insert(s, element):
-    # Your code here
-    return s.add(element)
-    
-    
-    
-    
 
-# Function to remove element from set
-# No return and nothing to print
+     #your code here
+     s.add(element)
+
 def remove_from_set(s, element):
-    # Your code here
-    return s.remove(element)
-    
-    
-    
-    
-# Function to find sum of elements in set
-# return value should be there as sum
+
+      #your code here
+     s.remove(element)
+
 def sum_set(s):
-    # Your code here
-    return sum(s)
-    
-    
+
+     #your code here
+     return sum(s)
