2a8bcb1670f64a9698957687da101e7616811cbf
Manaswi <155607245+Manaswikaki@users.noreply.github.com>
2025-10-07 21:24:15 +0530

    Time: 1162 ms (96.9%), Space: 0B (100%) - LeetHub

---
+++ 0607-sales-person/0607-sales-person.sql
@@ -0,0 +1,9 @@
+-- Option 2: NOT IN (less robust if there are NULLs)
+SELECT s.name
+FROM SalesPerson s
+WHERE s.sales_id NOT IN (
+    SELECT o.sales_id
+    FROM Orders o
+    JOIN Company c ON o.com_id = c.com_id
+    WHERE c.name = 'RED'
+);
