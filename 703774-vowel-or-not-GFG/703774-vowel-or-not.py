23e7530f9f226e263c2b04a68ddd81ba35f26428
Manaswi <155607245+Manaswikaki@users.noreply.github.com>
2026-02-15 12:21:18 +0530

    Added solution - LeetHub

---
+++ 703774-vowel-or-not-GFG/703774-vowel-or-not.py
@@ -0,0 +1,9 @@
+#User function Template for python3
+class Solution:
+    def isVowel (ob,c):
+        # code here 
+        vow=['A','E','I','O','U','a','e','i','o','u']
+        if c in vow:
+            return "YES"
+        else:
+            return "NO"
