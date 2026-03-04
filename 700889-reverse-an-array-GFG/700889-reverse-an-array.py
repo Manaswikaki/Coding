06242afbe18e1fe1e215f1d741ffa824bbd8f567
Manaswi <155607245+Manaswikaki@users.noreply.github.com>
2026-03-04 21:58:59 +0530

    Added solution - LeetHub

---
+++ 700889-reverse-an-array-GFG/700889-reverse-an-array.py
@@ -0,0 +1,9 @@
+class Solution:
+    def reverseArray(self, arr):
+        # In-place reversal using two pointers
+        left, right = 0, len(arr) - 1
+        while left < right:
+            arr[left], arr[right] = arr[right], arr[left]
+            left += 1
+            right -= 1
+        return arr
