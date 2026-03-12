24b1acd91035b38109cd80fbaeba6e5f4a16c8b6
Manaswi <155607245+Manaswikaki@users.noreply.github.com>
2026-03-12 20:59:22 +0530

    Time: 7 ms (86.42%), Space: 19.2 MB (94.53%) - LeetHub

---
+++ 0006-zigzag-conversion/0006-zigzag-conversion.py
@@ -0,0 +1,19 @@
+class Solution:
+    def convert(self, s: str, numRows: int) -> str:
+        if numRows <= 1 or numRows >= len(s):
+            return s
+
+        # Create a list of strings for each row
+        rows = [""] * numRows
+
+        cur_row = 0
+        going_down = False  # direction flag
+
+        for ch in s:
+            rows[cur_row] += ch
+            # Change direction when we reach the top or bottom row
+            if cur_row == 0 or cur_row == numRows - 1:
+                going_down = not going_down
+            cur_row += 1 if going_down else -1
+
+        return "".join(rows)
