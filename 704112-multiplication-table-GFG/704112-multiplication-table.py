9765ab0645b95586be9d4540bbaf9a68f3412e04
Manaswi <155607245+Manaswikaki@users.noreply.github.com>
2026-04-21 23:20:35 +0530

    Added solution - LeetHub

---
+++ 704112-multiplication-table-GFG/704112-multiplication-table.py
@@ -0,0 +1,9 @@
+#User function Template for python3
+
+class Solution:
+    def getTable(self,n):
+        # code here
+        a=[]
+        for i in range(1,11):
+            a.append(n*i)
+        return a    
