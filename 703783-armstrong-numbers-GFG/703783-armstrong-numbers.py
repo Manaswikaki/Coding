c4527af0fac38c9ff41dff9462396b55b02946c0
Manaswi <155607245+Manaswikaki@users.noreply.github.com>
2026-06-05 23:50:09 +0530

    Added solution - LeetHub

---
+++ 703783-armstrong-numbers-GFG/703783-armstrong-numbers.py
@@ -0,0 +1,19 @@
+#User function Template for python3
+
+class Solution:
+    def armstrongNumber (self, n):
+        # code here 
+        a=n
+        s=0
+        while n!=0:
+            r=n%10
+            s=s+(r*r*r)
+            n=n//10
+        if s==a:
+            return True
+        else:
+            return False
+
+# Synced seamlessly with LeetHub Pro
+# Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+# Get it here: https://chromewebstore.google.com/detail/leethub-v4/bcilpkkbokcopmabingnndookdogmbna
