c9c56a78ca2517e67b6ad000179bfe94ea16a688
Manaswi <155607245+Manaswikaki@users.noreply.github.com>
2026-06-06 00:29:58 +0530

    Time: 51 ms (47.07%), Space: 19.2 MB (18.45%) - LeetHub - Time: O(?), Space: O(?)

---
+++ 0509-fibonacci-number/0509-fibonacci-number.py
@@ -0,0 +1,13 @@
+class Solution:
+    def fib(self, n: int) -> int:
+        x=0
+        b=1
+        l=[]
+        for i in range(n):
+            l.append(x)
+            x,b=b,x+b
+        return x
+
+# Synced seamlessly with LeetHub Pro
+# Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+# Get it here: https://chromewebstore.google.com/detail/leethub-v4/bcilpkkbokcopmabingnndookdogmbna
