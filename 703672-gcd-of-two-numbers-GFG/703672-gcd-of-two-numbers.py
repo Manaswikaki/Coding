9032ee56348e450c1d9e4400b75ba9eb59b79d50
Manaswi <155607245+Manaswikaki@users.noreply.github.com>
2026-06-05 23:57:45 +0530

    Added solution - LeetHub

---
+++ 703672-gcd-of-two-numbers-GFG/703672-gcd-of-two-numbers.py
@@ -0,0 +1,10 @@
+class Solution:
+    def gcd(self, a, b):
+        while b:
+            a, b = b, a % b
+        return a
+
+
+# Synced seamlessly with LeetHub Pro
+# Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+# Get it here: https://chromewebstore.google.com/detail/leethub-v4/bcilpkkbokcopmabingnndookdogmbna
