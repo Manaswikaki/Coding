fe8647fb0277e4eb680a79b4bff9cf35b499e380
Manaswi <155607245+Manaswikaki@users.noreply.github.com>
2026-06-05 23:44:24 +0530

    Added solution - LeetHub

--- 703954-prime-number-GFG/703954-prime-number.py
+++ 703954-prime-number-GFG/703954-prime-number.py
@@ -1,14 +1,25 @@
 class Solution:
     def isPrime(self, n):
-        # 1 and below are not prime numbers
+        # 1 or less are not prime
         if n <= 1:
             return False
+        # 2 and 3 are prime
+        if n <= 3:
+            return True
+        # Eliminate multiples of 2 and 3
+        if n % 2 == 0 or n % 3 == 0:
+            return False
         
-        # Check for divisors from 2 to sqrt(n)
-        i = 2
+        # Check from 5 to sqrt(n), skipping even numbers
+        i = 5
         while i * i <= n:
-            if n % i == 0:
+            if n % i == 0 or n % (i + 2) == 0:
                 return False
-            i += 1
+            i += 6
             
         return True
+
+
+# Synced seamlessly with LeetHub Pro
+# Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+# Get it here: https://chromewebstore.google.com/detail/leethub-v4/bcilpkkbokcopmabingnndookdogmbna
