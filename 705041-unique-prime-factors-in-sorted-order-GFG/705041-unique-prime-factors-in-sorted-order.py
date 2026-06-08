ef75e68a2ff82d60c93bc3590310fd5a26c60090
Manaswi <155607245+Manaswikaki@users.noreply.github.com>
2026-06-08 14:14:25 +0530

    Added solution - LeetHub

--- 705041-unique-prime-factors-in-sorted-order-GFG/705041-unique-prime-factors-in-sorted-order.py
+++ 705041-unique-prime-factors-in-sorted-order-GFG/705041-unique-prime-factors-in-sorted-order.py
@@ -1,16 +1,16 @@
 class Solution:
     def primeFac(self, n):
         # code here
-        fact=set()
+        fact=[]
         while n%2==0:
-            fact.add(2)
+            fact.append(2)
             n//=2
         for i in range(3,int(n**0.5)+1,2):
             while n%i==0:
-                fact.add(i)
+                fact.append(i)
                 n//=i
         if n>2:
-            fact.add(n)
+            fact.append(n)
         res=list(set(fact))
         res.sort()
         return res
