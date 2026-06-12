c8a604bd9caeb716c789e3e6546a4fb44f8b7d53
Manaswi <155607245+Manaswikaki@users.noreply.github.com>
2026-06-12 10:54:47 +0530

    Time: 3 ms (45.41%), Space: 20.5 MB (78.67%) - LeetHub - Time: O(n), Space: O(1)

--- 0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.py
+++ 0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.py
@@ -1,13 +1,20 @@
-from typing import List
-
 class Solution:
     def removeDuplicates(self, nums: List[int]) -> int:
         if not nums:
             return 0
+        
+        # k tracks the index where the next unique element should go
+        k = 1 
+        
+        for i in range(1, len(nums)):
+            # If the current element is different from the previous one, it's unique
+            if nums[i] != nums[i - 1]:
+                nums[k] = nums[i]
+                k += 1
+                
+        return k
 
-        i = 0  # index of last unique element
-        for j in range(1, len(nums)):
-            if nums[j] != nums[i]:
-                i += 1
-                nums[i] = nums[j]
-        return i + 1
+
+# Synced seamlessly with LeetHub Pro
+# Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+# Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
