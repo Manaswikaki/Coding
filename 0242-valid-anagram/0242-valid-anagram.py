9c5b2a0046982ee84781d70b23dc9d39e3383bc4
Manaswi <155607245+Manaswikaki@users.noreply.github.com>
2026-06-21 19:03:27 +0530

    Time: 11 ms (76.66%), Space: 19.3 MB (75.3%) - LeetHub - Time: O(n), Space: O(n)

---
+++ 0242-valid-anagram/0242-valid-anagram.py
@@ -0,0 +1,18 @@
+class Solution:
+    def isAnagram(self, s: str, t: str) -> bool:
+        # If lengths are different, they can't be anagrams
+        if len(s) != len(t):
+            return False
+        
+        countS, countT = {}, {}
+        
+        for i in range(len(s)):
+            countS[s[i]] = 1 + countS.get(s[i], 0)
+            countT[t[i]] = 1 + countT.get(t[i], 0)
+            
+        return countS == countT
+
+
+# Synced seamlessly with LeetHub Pro
+# Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+# Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
