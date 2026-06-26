451dcea51357ce69bb521512d497f68f2f423e4e
Manaswi <155607245+Manaswikaki@users.noreply.github.com>
2026-06-26 16:23:21 +0530

    Time: 71 ms (54.1%), Space: 19.8 MB (36.7%) - LeetHub

---
+++ 0076-minimum-window-substring/0076-minimum-window-substring.py
@@ -0,0 +1,51 @@
+from collections import Counter
+
+class Solution:
+    def minWindow(self, s: str, t: str) -> str:
+        if not t or not s:
+            return ""
+
+        # Dictionary which keeps a count of all the unique characters in t.
+        dict_t = Counter(t)
+        required = len(dict_t)
+
+        # Left and Right pointer
+        l, r = 0, 0
+
+        # formed is used to keep track of how many unique characters in t 
+        # are present in the current window in its desired frequency.
+        formed = 0
+        window_counts = {}
+
+        # ans tuple of the form (window length, left, right)
+        ans = float("inf"), None, None
+
+        while r < len(s):
+            character = s[r]
+            window_counts[character] = window_counts.get(character, 0) + 1
+
+            if character in dict_t and window_counts[character] == dict_t[character]:
+                formed += 1
+
+            # Try and contract the window till the point where it ceases to be 'desirable'.
+            while l <= r and formed == required:
+                character = s[l]
+
+                # Save the smallest window until now.
+                if r - l + 1 < ans[0]:
+                    ans = (r - l + 1, l, r)
+
+                window_counts[character] -= 1
+                if character in dict_t and window_counts[character] < dict_t[character]:
+                    formed -= 1
+
+                l += 1    
+
+            r += 1    
+            
+        return "" if ans[0] == float("inf") else s[ans[1] : ans[2] + 1]
+
+
+# Synced seamlessly with LeetHub Pro
+# Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+# Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
