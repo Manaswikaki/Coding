f7e3e9e58a99980263e0fd087cece6aac1c6b5b1
Manaswi <155607245+Manaswikaki@users.noreply.github.com>
2026-07-04 19:29:23 +0530

    Time: 36 ms (95.92%), Space: 19.1 MB (64.2%) - LeetHub - Time: O(?), Space: O(?)

--- 0190-reverse-bits/0190-reverse-bits.py
+++ 0190-reverse-bits/0190-reverse-bits.py
@@ -2,6 +2,13 @@ class Solution:
     def reverseBits(self, n: int) -> int:
         res = 0
         for _ in range(32):
+            # Shift res left to make room for the next bit
             res = (res << 1) | (n & 1)
+            # Shift n right to process the next bit
             n >>= 1
-        return res & 0xFFFFFFFF
+        return res
+
+
+# Synced seamlessly with LeetHub Pro
+# Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+# Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
