eec6bb43febe7071ac852f9196db114ecaff5052
Manaswi <155607245+Manaswikaki@users.noreply.github.com>
2026-07-08 22:38:33 +0530

    Added solution - LeetHub

---
+++ 703457-min-and-max-in-array-GFG/703457-min-and-max-in-array.py
@@ -0,0 +1,10 @@
+class Solution:
+    def getMinMax(self, arr):
+        # min() returns the smallest element in the array
+        # max() returns the largest element in the array
+        return min(arr), max(arr)
+
+
+# Synced seamlessly with LeetHub Pro
+# Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+# Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
