c576d0533b3e87a9a06e92e8263ebcb0a44be7f8
Manaswi <155607245+Manaswikaki@users.noreply.github.com>
2026-07-11 23:36:52 +0530

    Time: 118 ms (0.63%), Space: 20 MB (42.72%) - LeetHub

--- 0035-search-insert-position/0035-search-insert-position.py
+++ 0035-search-insert-position/0035-search-insert-position.py
@@ -1,8 +1,18 @@
-from typing import List
-
 class Solution:
     def searchInsert(self, nums: List[int], target: int) -> int:
-        for i in range(len(nums)):
-            if nums[i] >= target:
-                return i
-        return len(nums)
+        x=len(nums)
+        for i in range(x):
+            if target in nums:
+                if nums[i]==target:
+                    return i
+            else:
+                if nums[x-1]<target:
+                    return x
+                else:
+                    if nums[i]>target:
+                        return i
+                
+
+# Synced seamlessly with LeetHub Pro
+# Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+# Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
