c5b65d3c24cdede24e9ff4090bb1a5a4ceac44a3
Manaswi <155607245+Manaswikaki@users.noreply.github.com>
2026-07-12 22:23:25 +0530

    Time: 2 ms (9.06%), Space: 20.5 MB (59.34%) - LeetHub

--- 0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.py
+++ 0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.py
@@ -19,3 +19,8 @@ class Solution:
             return bound
 
         return [findBound(True), findBound(False)]
+
+
+# Synced seamlessly with LeetHub Pro
+# Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+# Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
