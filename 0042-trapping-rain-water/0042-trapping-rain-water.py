c7a82dd82c55abdad875eeab82027e2204b90dd3
Manaswi <155607245+Manaswikaki@users.noreply.github.com>
2026-07-23 22:57:02 +0530

    Time: 11 ms (45.11%), Space: 20.9 MB (83.82%) - LeetHub - Time: O(n), Space: O(1)

--- 0042-trapping-rain-water/0042-trapping-rain-water.py
+++ 0042-trapping-rain-water/0042-trapping-rain-water.py
@@ -18,3 +18,8 @@ class Solution:
                 water += right_max - height[right]
                 
         return water
+
+
+# Synced seamlessly with LeetHub Pro
+# Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+# Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
