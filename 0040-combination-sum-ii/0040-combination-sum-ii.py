5207c0488694eb315142d4b07b6627f9f4eff7b4
Manaswi <155607245+Manaswikaki@users.noreply.github.com>
2026-08-30 20:22:10 +0530

    Time: 3 ms (84.91%), Space: 19.5 MB (43.46%) - LeetHub - Time: O(2^N * N), Space: O(N)

--- 0040-combination-sum-ii/0040-combination-sum-ii.py
+++ 0040-combination-sum-ii/0040-combination-sum-ii.py
@@ -26,3 +26,8 @@ class Solution:
 
         backtrack(target, 0, [])
         return res
+
+
+# Synced seamlessly with LeetHub Pro
+# Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+# Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
