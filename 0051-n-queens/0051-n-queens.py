df640943d8bdb08e8935aa1c2026b89cf67e23d8
Manaswi <155607245+Manaswikaki@users.noreply.github.com>
2026-09-03 21:36:49 +0530

    Time: 7 ms (96.85%), Space: 19.8 MB (35.35%) - LeetHub - Time: O(N!), Space: O(N^2)

---
+++ 0051-n-queens/0051-n-queens.py
@@ -0,0 +1,42 @@
+class Solution:
+    def solveNQueens(self, n: int) -> list[list[str]]:
+        res = []
+        board = [["."] * n for _ in range(n)]
+        
+        # Sets to keep track of attacked columns and diagonals
+        cols = set()
+        posDiag = set() # (r + c)
+        negDiag = set() # (r - c)
+        
+        def backtrack(r):
+            if r == n:
+                copy = ["".join(row) for row in board]
+                res.append(copy)
+                return
+            
+            for c in range(n):
+                if c in cols or (r + c) in posDiag or (r - c) in negDiag:
+                    continue
+                
+                # Place the queen
+                cols.add(c)
+                posDiag.add(r + c)
+                negDiag.add(r - c)
+                board[r][c] = "Q"
+                
+                # Move to the next row
+                backtrack(r + 1)
+                
+                # Undo the placement (backtrack)
+                cols.remove(c)
+                posDiag.remove(r + c)
+                negDiag.remove(r - c)
+                board[r][c] = "."
+                
+        backtrack(0)
+        return res
+
+
+# Synced seamlessly with LeetHub Pro
+# Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+# Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
