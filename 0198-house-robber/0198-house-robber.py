1ed369f58560bf5744885f73187a585cfa9c8bab
Manaswi <155607245+Manaswikaki@users.noreply.github.com>
2026-09-15 21:55:10 +0530

    Time: 0 ms (100%), Space: 19.2 MB (90.43%) - LeetHub - Time: O(n), Space: O(1)

---
+++ 0198-house-robber/0198-house-robber.py
@@ -0,0 +1,16 @@
+class Solution:
+    def rob(self, nums: List[int]) -> int:
+        rob1, rob2 = 0, 0
+        
+        # [rob1, rob2, n, n+1, ...]
+        for n in nums:
+            temp = max(n + rob1, rob2)
+            rob1 = rob2
+            rob2 = temp
+            
+        return rob2
+
+
+# Synced seamlessly with LeetHub Pro
+# Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+# Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
